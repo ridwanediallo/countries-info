155729cdaa7d97b0789c74cea337c5f92b33733e
ridwan <ridwanediallo99@gmail.com>
2022-05-24 17:31:06 +0000

    Connect redux to react

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { getCountriesData } from '../redux/CountriesSlice';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+      const dispatch = useDispatch();
+      const countries = useSelector((state) => state.allCountries);
+      console.log(countries);
 
-export default Home
+      useEffect(() => {
+        dispatch(getCountriesData());
+      }, [dispatch]);
+
+  return <div>Home</div>;
+};
+
+export default Home;
